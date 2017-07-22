a9b2fbfccdc09ab0c63893e2771ef871bd0fef89
Unknown <phillipsic@gmail.com>
2017-07-22 21:49:49 +0200

    Now loading users from collection

--- imports/ui/bodys.js
+++ imports/ui/bodys.js
@@ -4,15 +4,15 @@ import './body.html';
 import './projectadmin_template.html';
 import './useradmin_template.html';
 import './mysessions_template.html';
-  
-//import { Users } from '../api/users.js';
+//import { users } from '../api/users.js';
 
 BlazeLayout.setRoot('#test');
 
 Template.usersAdmin.helpers({ 
-  users: [ 
-    { text: 'user1' }, 
-    { text: 'user2' }, 
-    { text: 'user3' }, 
-  ], 
+	  listusers() {
+
+		  console.log(Meteor.users.find({}))
+		    return Meteor.users.find({});
+
+		  },
 });
