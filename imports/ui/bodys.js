57641e5ff3a56b9dbc56031586a189ba318fbc9e
Unknown <phillipsic@gmail.com>
2017-07-29 21:42:05 +0200

    Added insert new user

--- imports/ui/bodys.js
+++ imports/ui/bodys.js
@@ -17,3 +17,16 @@ Template.usersAdmin.helpers({
 
 		  },
 });
+
+
+Template.usersAdmin.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        });
+    }
+});
