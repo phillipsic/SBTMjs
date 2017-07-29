b79db43aae743e761a716715ae0ae1f1a6f06b37
Unknown <phillipsic@gmail.com>
2017-07-30 00:43:53 +0200

    Changed login and implemented logout

--- imports/ui/bodys.js
+++ imports/ui/bodys.js
@@ -5,6 +5,7 @@ import './projectadmin_template.html';
 import './useradmin_template.html';
 import './mysessions_template.html';
 import './dashboard_template.html';
+import './login_template.html';
 // import { users } from '../api/users.js';
 
 BlazeLayout.setRoot('#test');
@@ -17,6 +18,32 @@ Template.usersAdmin.helpers({
 
 		  },
 });
+
+Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(email, password);
+    }
+});
+
+Template.adminmenubar.events({
+    'click  .glyphicon-log-out': function(event){
+    	console.log("Initiated the logout process")
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
+
+Template.adminmenubar.events({
+    'click  .logout': function(event){
+    	console.log("Initiated the logout process")
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
+
 
 
 Template.usersAdmin.events({
@@ -64,7 +91,7 @@ Template.usersAdmin.events({
         var roles = ['admin', 'roleName']; 
        // Roles.addUsersToRoles(id, roles);
 	
-        	Roles.addUsersToRoles(Meteor.userId(), 'admin', 'default-group');
+        	Roles.addUsersToRoles(Meteor.userId(), role, 'default-group');
         // }}
         
     }
